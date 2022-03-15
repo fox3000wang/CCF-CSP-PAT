85cb5bd70661a45c436966cc426bb201648a6512
fox.wang <fox.wang@saybot.com>
2022-03-15 10:37:49 +0800

    折点计数

---
+++ CCF-201604-1.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+int main()
+{
+  freopen("CCF-201604-1.txt", "r", stdin);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+
+  gg n, o = 0;
+  cin >> n;
+  gg m = n - 1;
+  gg num[n], res[m];
+
+  for (gg i = 0; i < n; i++)
+  {
+    cin >> num[i];
+  }
+
+  for (gg i = 1; i < n; i++)
+  {
+    res[i - 1] = num[i] - num[i - 1];
+  }
+
+  for (gg i = 1; i < m; i++)
+  {
+    if ((res[i] > 0 && res[i - 1] < 0) || (res[i] < 0 && res[i - 1] > 0)) // bad code
+    {
+      o++;
+    }
+  }
+
+  cout << o;
+  return 0;
+}
