f8a6c68b7196fba97079d1e6a8bd12814ffc4d46
fox.wang <fox.wang@saybot.com>
2022-03-15 18:57:41 +0800

    打酱油

--- CCF-1/CCF-201709-1.cpp
+++ CCF-1/CCF-201709-1.cpp
@@ -4,12 +4,11 @@ using namespace std;
 using gg = long long;
 int main()
 {
-  freopen("CCF-201312-1.txt", "r", stdin);
+  freopen("CCF-201709-1.txt", "r", stdin);
   // ios::sync_with_stdio(false);
   // cin.tie(0);
-  gg n, o = 0;
+  gg n;
   cin >> n;
-
-  cout << o;
+  cout << n / 10 + (n / 50 * 2) + (n % 50 / 30);
   return 0;
 }
