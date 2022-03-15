1d8487142f9aebd9b4a53af3272bcd985227057e
fox.wang <fox.wang@saybot.com>
2022-03-15 10:05:04 +0800

    数位之和

---
+++ CCF-201512-1.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+int main()
+{
+  freopen("CCF-201512-1.txt", "r", stdin);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+
+  gg o = 0;
+  string n;
+  cin >> n;
+  for (gg i = 0; i < n.size(); i++)
+  {
+    o += (n[i] - '0');
+  }
+  cout << o;
+  return 0;
+}
