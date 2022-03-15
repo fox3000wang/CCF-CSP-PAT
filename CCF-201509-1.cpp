748398628fa6946ecd1ead504cc4572c407e40a9
fox.wang <fox.wang@saybot.com>
2022-03-15 09:41:48 +0800

    优化代码

--- CCF-201509-1.cpp
+++ CCF-201509-1.cpp
@@ -8,16 +8,12 @@ int main()
   // ios::sync_with_stdio(false);
   // cin.tie(0);
 
-  gg n, o = 1;
+  gg n, o = 0;
   cin >> n;
   gg num[n];
-
   for (gg i = 0; i < n; i++)
   {
     cin >> num[i];
-  }
-  for (gg i = 1; i < n; i++)
-  {
     if (num[i] != num[i - 1])
     {
       o++;
