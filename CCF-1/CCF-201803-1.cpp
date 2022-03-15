37af32891deb2c7d3f6f8ea912bda630e656bbd3
fox.wang <fox.wang@saybot.com>
2022-03-15 20:39:38 +0800

    跳一跳

---
+++ CCF-1/CCF-201803-1.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+int main()
+{
+  freopen("CCF-201803-1.txt", "r", stdin);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+
+  gg n = -1, last = 0, o = 0;
+  while (n != 0)
+  {
+    cin >> n;
+    if (n == 1)
+    {
+      o++;
+      last = 0;
+    }
+    if (n == 2)
+    {
+      o += 2;
+      o += last;
+      last += 2;
+    }
+  }
+
+  cout << o;
+  return 0;
+}
