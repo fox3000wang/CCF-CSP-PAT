ed14482208d1143e3ec65f3ae9abc1db049a4421
fox.wang <fox.wang@saybot.com>
2022-03-16 10:10:01 +0800

    小明上学

---
+++ CCF-1/CCF-201812-1.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+int main()
+{
+  freopen("CCF-201812-1.txt", "r", stdin);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+  gg r, y, g, n, k, t, o = 0;
+  cin >> r >> y >> g >> n;
+
+  for (gg i = 0; i < n; i++)
+  {
+    cin >> k >> t;
+    if (k == 0 or k == 1)
+    {
+      o += t;
+    }
+    if (k == 2)
+    {
+      o += (t + r);
+    }
+  }
+
+  cout << o;
+  return 0;
+}
