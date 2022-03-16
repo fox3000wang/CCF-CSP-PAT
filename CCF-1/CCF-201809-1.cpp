02d1a53de75468cfbf915330d354cb5b55025086
fox.wang <fox.wang@saybot.com>
2022-03-16 08:59:04 +0800

    卖菜

---
+++ CCF-1/CCF-201809-1.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+int main()
+{
+  freopen("CCF-201809-1.txt", "r", stdin);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+  gg n;
+  cin >> n;
+  gg num[n], ans[n];
+
+  for (gg i = 0; i < n; i++)
+  {
+    cin >> num[i];
+  }
+
+  ans[0] = (num[0] + num[1]) / 2;
+  ans[n - 1] = (num[n - 1] + num[n - 2]) / 2;
+
+  for (gg i = 1; i < (n - 1); i++)
+  {
+    ans[i] = (num[i - 1] + num[i] + num[i + 1]) / 3;
+  }
+
+  for (gg i = 0; i < n; i++)
+  {
+    cout << ans[i] << " ";
+  }
+
+  return 0;
+}
