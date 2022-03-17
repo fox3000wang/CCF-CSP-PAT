7be6d36f3c75c832c2c2b70c8284fbd8dba809e5
fox.wang <fox.wang@saybot.com>
2022-03-17 10:06:30 +0800

    中位数

---
+++ CCF-1/CCF-201612-1.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+int main()
+{
+  freopen("CCF-201612-1.txt", "r", stdin);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+  gg n, o = 0;
+  cin >> n;
+
+  vector<gg> ai(n);
+  for (gg &i : ai)
+  {
+    cin >> i;
+  }
+  sort(ai.begin(), ai.end());
+
+  gg t = ai[ai.size() / 2];
+  auto p = equal_range(ai.begin(), ai.end(), t);
+  o = (p.first - ai.begin() == ai.end() - p.second ? t : -1);
+  cout << o;
+  return 0;
+}
