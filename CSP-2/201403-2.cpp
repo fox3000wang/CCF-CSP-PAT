fda6946856213cb0696a3f44a8c7c7bb7ad08db7
fox <fox3000wang@hotmail.com>
2022-06-21 09:07:30 +0800

    update

--- CSP-2/201403-2.cpp
+++ CSP-2/201403-2.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 using gg = long long;
+
+// mian
 int main()
 {
   freopen("201403-2.txt", "r", stdin);
@@ -8,15 +10,50 @@ int main()
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  gg n, o = 0;
-  cin >> n;
+  gg n; // 窗口数
+  gg m; // 点击次数
+  cin >> n >> m;
+  vector<gg> o(m);
 
-  vector<gg> ai(n);
-  for (gg &i : ai)
+  vector<vector<gg>> w(n); // 窗口坐标
+  for (gg i = 0; i < n; i++)
   {
-    cin >> i;
+    w[i].resize(4);
+    cin >> w[i][0] >> w[i][1] >> w[i][2] >> w[i][3];
   }
 
-  cout << o;
+  vector<vector<gg>> c(m); // 点击坐标
+  for (gg i = 0; i < m; i++)
+  {
+    c[i].resize(2);
+    cin >> c[i][0] >> c[i][1];
+  }
+
+  for (gg i = 0; i < m; i++)
+  {
+    o[i] = 0;
+    for (gg j = n - 1; j >= 0; j--)
+    {
+      if (w[j][0] <= c[i][0] && c[i][0] <= w[j][2] && w[j][1] <= c[i][1] && c[i][1] <= w[j][3])
+      {
+        if (o[i] == 0)
+        {
+          o[i] = j + 1;
+        }
+      }
+    }
+    if (o[i] == 0)
+    {
+      cout << "IGNORED";
+    }
+    else
+    {
+      cout << o[i];
+    }
+    cout << "\n";
+  }
+
   return 0;
 }
+
+// 生成九九乘法表
